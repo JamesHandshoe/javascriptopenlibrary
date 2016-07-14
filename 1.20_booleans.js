b5d7a0b7e1f2b1f95a5070f409db47a69df51985
James <jhandshoe@sbcglobal.net>
2016-07-14 10:16:13 -0400

    added bools file to library

---
+++ 1.20_booleans.js
@@ -0,0 +1,10 @@
+//booleans
+//change gameState to do the different operations
+var gameState = false;
+var x = 2;
+if(gameState){
+	var x = 2 * 2
+	console.log(x);
+} else if(!gameState) {
+	console.log("cannot compute");
+}
