f869e24d33521e8784930041073db2ba5bfcb1c3
James <jhandshoe@sbcglobal.net>
2016-07-14 10:51:14 -0400

    added while loops to library

---
+++ 1.50_whileloops.js
@@ -0,0 +1,14 @@
+//while loops
+//loop through a string and print
+var x = 15;
+var message = "HOLLA BACK YA'LL"
+while(x > -1){
+	console.log(message[x]);
+	x = x -1;
+}
+
+var y = 0;
+while(y < 15){
+	console.log(message[y]);
+	y = y + 1;
+}
