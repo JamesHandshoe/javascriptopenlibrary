91e41ca618d3ddf88bf5042d971dd024170f0da0
James <jhandshoe@sbcglobal.net>
2016-07-14 11:24:43 -0400

    added arrays to library

---
+++ 1.60_array.js
@@ -0,0 +1,19 @@
+//arrays
+
+var colors = ["white", "black", "red", "orange", "green", "blue", "indigo", "violet"];
+
+//console log different colors
+
+console.log(colors[0]);
+console.log(colors[5]);
+
+//add an item to colors **goes to the end**
+console.log(colors);
+colors.push('pink');
+console.log(colors);
+
+//delete an item from colors **deletes the last item**
+
+console.log(colors);
+colors.pop();
+console.log(colors);
